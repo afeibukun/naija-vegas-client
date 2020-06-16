8c8cdc5f77838855c81db16a52017338d5f2fbeb
Ibukunoluwa Afe <afeibukun@gmail.com>
2020-06-16 23:39:57 +0100

    category page update

--- src/views/sports.jsx
+++ src/views/sports.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { SportsBetDropDown } from "../components/exports";
+import Footer from "../components/footers/mainFooter";
+
 const Sports = () => {
   return (
-    <div className="politics-bg">
-      <h1 className="pageHeader py-4 pl-4">Sport</h1>
-      <section>
-        <SportsBetDropDown id={1} betName={"Today"} />
-        <SportsBetDropDown id={2} betName={"Champions League"} />
-        <SportsBetDropDown id={3} betName={"Premier League"} />
-        <SportsBetDropDown id={4} betName={"La Liga"} />
-      </section>
+    <div className="politics-bg ">
+      <div className="container">
+        <section className="section">
+          <h1 className="pageHeader py-4 pl-4">Sport</h1>
+          <div>
+            <SportsBetDropDown id={1} betName={"Today"} />
+            <SportsBetDropDown id={2} betName={"Champions League"} />
+            <SportsBetDropDown id={3} betName={"Premier League"} />
+            <SportsBetDropDown id={4} betName={"La Liga"} />
+          </div>
+        </section>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
